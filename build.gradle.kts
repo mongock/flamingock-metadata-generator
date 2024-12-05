eaf8711f6e044879400c5a09972ab3838c1f7774
Antonio Perez Dieppa <aperezdieppa@gmail.com>
2024-12-05 12:08:48 +0000

    fix publishing duplications

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,14 +19,6 @@ repositories {
 }
 
 publishing {
-    publications {
-        create<MavenPublication>("mavenJava") {
-            from(components["java"])
-            groupId = project.group.toString()
-            artifactId = project.name
-            version = project.version.toString()
-        }
-    }
     repositories {
         mavenLocal()
     }
